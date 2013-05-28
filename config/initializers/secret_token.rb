89bd4eb7274930c4fafd0979b537d95fa7df90af
Grant M. Steerzer <GSteerzer@gmail.com>
2013-05-28 16:45:06 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govtinaction::Application.config.secret_token = '4c3ee1b89d5b60ee9561a05eed59e8b8b8fd91ecb4462093512c4a1e1bce0d8274a9376e9f14db34225eb960463d07d9a3ade2779cb2962f2861727b3f7cd167'
