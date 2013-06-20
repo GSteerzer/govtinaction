fe97b9354f4007758c2d2117c2f7c56fa09da126
Grant M. Steerzer <GSteerzer@gmail.com>
2013-06-20 14:15:41 -0400

    Devise Changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Govtinaction::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Govtinaction::Application.routes.draw do
+  devise_for :users
+
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +52,9 @@ Govtinaction::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of yoroot :to => 'welcome#index'ur site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # 
 
   # See how all your routes lay out with "rake routes"
 
